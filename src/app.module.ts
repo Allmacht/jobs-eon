26614d92d929d4d150e3966090f0b93ee428f1dc
Allmacht <ulises.jacob.cr@gmail.com>
2023-04-27 01:35:47 -0600

    add config module and DB conection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ShipmentsModule } from './shipments/shipments.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { Shipment } from './shipments/shipments.entity';
+import { Charge } from './charge/charge.entity';
+import { ChargeModule } from './charge/charge.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'intechslpaws.czg95ktujwr4.us-east-2.rds.amazonaws.com',
-      port: 3306,
-      username: 'intechslpaws',
-      password: 'intechslpaws',
-      database: 'eon_development',
-      entities: [Shipment],
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [Shipment, Charge],
       synchronize: false,
     }),
     ShipmentsModule,
+    ChargeModule,
   ],
   controllers: [],
   providers: [],
